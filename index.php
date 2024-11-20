15efd6183d36946f8d664ba7aadfee8a45e8eabe
FELIPE PIRES DOS SANTOS <felipe.psantos48@senacsp.edu.br>
2024-11-19 22:04:53 -0300

    Terceiro Commit

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Ipobre</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </head>
-  <body>
+  <body style="background-image:url('Imagem/fundo-1.png');background-size:100%;">
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Ipobre</a>
@@ -78,8 +79,46 @@
     </div>
   </div>
 </nav>
+  <br><br><br><br><br><br>
+
+       
+<div class="container text-center">
+                      <div class="row">
+                        <div class="col">
+                         <div class="card" style="width: 18rem;">
+                              <img src="Imagem/GalaxyPocket.png" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                <h5 class="card-title">Galaxy Pocket </h5>
+                                <p class="card-text">Smartphone Samsung Galaxy Pocket / prateado</p>
+                                <a href="#" class="btn btn-primary">Comprar</a>
+                              </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="Imagem/nokiatijolao.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Nokia 310</h5>
+                              <p class="card-text">Smartphone Nokia 310 <br> muito resistente</p>
+                              <a href="#" class="btn btn-primary">Comprar</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+                        <div class="card" style="width: 18rem;">
+                          <img src="Imagem/sonyericsson.png" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title"><br><br>Sony Ericsson</h5>
+                            <p class="card-text">Smartphone Sony Ericsson <br><br> Resistente e bonito <br> </p>
+                            <a href="#" class="btn btn-primary"> Comprar</a>
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+  </div>
  
  
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
 </html>
  
